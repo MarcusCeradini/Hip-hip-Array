6f411a4665525c5cd889564b19addefcea25a969
MarcusCeradini <mnceradini@my.waketech.edu>
2025-10-10 19:56:06 -0400

    Update Shuffle.java

--- Shuffle.java
+++ Shuffle.java
@@ -4,20 +4,27 @@ Date: 10/2/2025
 Description: Prints Shuffle playlist
 */
 
-import java.util.Scanner;
-
-public class Shuffle{
+public class Shuffler{
     public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
 
-        String [] playlist = {"Shake It Off – Taylor Swift",
-                "I Love You – Barney",
-                "Baby Shark – Pinkfong",
-                "Never Gonna Give You Up – Rick Astley",
-                "Axel F (Crazy Frog) – Crazy Frog",
-                "Gangnam Style – PSY",
-                "What Does the Fox Say? – Ylvis"};
+        String [] playlist = {"Shake It Off - Taylor Swift",
+                "I Love You - Barney",
+                "Baby Shark - Pinkfong",
+                "Never Gonna Give You Up - Rick Astley",
+                "Axel F (Crazy Frog) - Crazy Frog",
+                "Gangnam Style - PSY",
+                "What Does the Fox Say? - Ylvis"};
 
+        Shuffle(playlist);
+
+        System.out.println("Shuffled Playlist: ");
+
+        for (String songs : playlist){
+            System.out.println(songs);
+        }
+    }
+
+    public static void Shuffle (String[] playlist) {
         for (int i = 0; i < playlist.length - 1; i++){
             int j = (int) (Math.random() * playlist.length);
 
@@ -25,13 +32,5 @@ public class Shuffle{
             playlist[i] = playlist[j];
             playlist[j] = temp;
         }
-
-        System.out.println("Shuffled Playlist: ");
-
-        for (String songs : playlist){
-            System.out.println(songs);
-        }
-
     }
-
 }
